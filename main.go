a125d101e56ce8030c08e4188dd9a571f72d0427
Achintha Sandaruwan <achinthasandaruwan517@gmail.com>
2025-12-08 17:43:33 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+var loan_amount  int
+
+print("Enter loan amount: ")
+fmt.Scan(&loan_amount)
+
+
+var interest_rate int
+
+print("Enter interest rate: ")
+fmt.Scan(&interest_rate)
+
+var yeasrs int
+
+print("Enter time period(years): ")
+fmt.Scan(&yeasrs)
+
+var installment_amount int
+
+installment_amount = (loan_amount * interest_rate * yeasrs / 100 + loan_amount) / (yeasrs * 12)
+fmt.Printf("Installment amount is: %d\n", installment_amount)
+
+
+}
